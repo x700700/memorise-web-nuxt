d1e5420d9c8438e7968318a8e7fbe14e8f650d3c
x700700@gmail.com <booli@feature.fm>
2019-05-11 05:13:11 +0300

    card transition phase 1

--- components/Play/Card.vue
+++ components/Play/Card.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="card-container" @click="rotateCard()">
-    {{ cardFront }}
+    <div class="card-front-container" :style="styleCardFront">
+      <div class="card-content">
+        Cat
+      </div>
+    </div>
+    <div v-if="!cardFront" class="card-back-container" :style="styleCardBack">
+      <div class="card-content">
+        חתול
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,18 +28,49 @@ export default {
       required: true,
     },
   },
-  methods: {
+  computed: {
+    styleCardFront() {
+      return {
+        maxWidth: this.cardFront ? '100%' : '0%',
+        opacity: this.cardFront ? 1 : 0,
+        transition: !this.cardFront ? 'max-width 0.3s ease-in-out, opacity 0s linear 0.2s' : 'max-width 0.3s ease-in-out, opacity 0s linear 0.1s',
+      };
+    },
+    styleCardBack() {
+      return {
+        maxWidth: !this.cardFront ? '100%' : '0%',
+        opacity: !this.cardFront ? 1 : 0,
+        transition: this.cardFront ? 'max-width 0.3s ease-in-out, opacity 0s linear 0.2s' : 'max-width 0.3s ease-in-out, opacity 0s linear 0.1s',
+      };
+    },
   },
 }
 </script>
 
 <style lang="less" scoped>
-
-.memo-header {
   @import '../../global/global-vars';
 
   .card-container {
+    height: 100%;
 
+    .card-front-container, .card-back-container {
+      height: 100%;
+      background-color: @grey-lightest;
+      font-size: 32px;
+      display: flex;
+
+      .card-content {
+        width: 100%;
+        text-align: center;
+        align-self: center;
+      }
+    }
+    .card-front-container {
+      margin-left: auto;
+    }
+    .card-back-container {
+      // transform: translateY(-100%);
+      background-color: @card-back-bg;
+    }
   }
-}
 </style>
