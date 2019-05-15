4e724c8752daf4e69d940aef6ade70fc21a2d56e
x700700@gmail.com <booli@feature.fm>
2019-05-15 22:00:07 +0300

    stubs

--- stubs/trainings.js
+++ stubs/trainings.js
@@ -1,58 +1,58 @@
 
 const trainingsStub = {
-  "5bc2eaf4d71c15c14d0ec30a": {
-    "id": "5bc2eaf4d71c15c14d0ec30a",
-    "name": "xdghdg",
-    "created": "2018-10-14T07:06:28.172Z",
-    "modified": "2018-10-14T07:06:50.666Z",
-    "popularity": 4,
-    "difficulty": 2,
-    "exercisesCount": 5,
-    "info": {
-      "userId": "5bc006e5457bb9bf8d7fe0a6",
-      "archived": false,
-      "lastTrainedAt": null
+  '5bc2eaf4d71c15c14d0ec30a': {
+    id: '5bc2eaf4d71c15c14d0ec30a',
+    name: 'xdghdg',
+    created: '2018-10-14T07:06:28.172Z',
+    modified: '2018-10-14T07:06:50.666Z',
+    popularity: 4,
+    difficulty: 2,
+    exercisesCount: 5,
+    info: {
+      userId: '5bc006e5457bb9bf8d7fe0a6',
+      archived: false,
+      lastTrainedAt: null
     },
-    "sampleExercise": {
-      "id": "5bc2eb05d71c15c14d0ec313",
-      "trainingId": "5bc2eaf4d71c15c14d0ec30a",
-      "question": "gfhg",
-      "answer": "ghgh",
-      "played": 0,
-      "wrongs": 0,
-      "shouldReloadSampleExercises": true,
-      "info": {
-        "created": "2018-10-14T07:06:45.253Z",
-        "modified": "2018-10-14T07:06:50.666Z",
-        "lastTrainedAt": null
+    sampleExercise: {
+      id: '5bc2eb05d71c15c14d0ec313',
+      trainingId: '5bc2eaf4d71c15c14d0ec30a',
+      question: 'gfhg',
+      answer: 'ghgh',
+      played: 0,
+      wrongs: 0,
+      shouldReloadSampleExercises: true,
+      info: {
+        created: '2018-10-14T07:06:45.253Z',
+        modified: '2018-10-14T07:06:50.666Z',
+        lastTrainedAt: null
       }
     }
   },
-  "5bc006f6457bb9bf8d7fe0a7": {
-    "id": "5bc006f6457bb9bf8d7fe0a7",
-    "name": "אוצ ״מ סוכות",
-    "created": "2018-10-12T02:29:10.185Z",
-    "modified": "2018-10-12T02:31:14.562Z",
-    "popularity": 4,
-    "difficulty": 2,
-    "exercisesCount": 8,
-    "info": {
-      "userId": "5bc006e5457bb9bf8d7fe0a6",
-      "archived": false,
-      "lastTrainedAt": null
+  '5bc006f6457bb9bf8d7fe0a7': {
+    id: '5bc006f6457bb9bf8d7fe0a7',
+    name: 'אוצ ״מ סוכות',
+    created: '2018-10-12T02:29:10.185Z',
+    modified: '2018-10-12T02:31:14.562Z',
+    popularity: 4,
+    difficulty: 2,
+    exercisesCount: 8,
+    info: {
+      userId: '5bc006e5457bb9bf8d7fe0a6',
+      archived: false,
+      lastTrainedAt: null
     },
-    "sampleExercise": {
-      "id": "5bc00732457bb9bf8d7fe0ae",
-      "trainingId": "5bc006f6457bb9bf8d7fe0a7",
-      "question": "אריה",
-      "answer": "לביאה",
-      "played": 0,
-      "wrongs": 0,
-      "shouldReloadSampleExercises": true,
-      "info": {
-        "created": "2018-10-12T02:30:10.370Z",
-        "modified": "2018-10-12T02:31:14.562Z",
-        "lastTrainedAt": null
+    sampleExercise: {
+      id: '5bc00732457bb9bf8d7fe0ae',
+      trainingId: '5bc006f6457bb9bf8d7fe0a7',
+      question: 'אריה',
+      answer: 'לביאה',
+      played: 0,
+      wrongs: 0,
+      shouldReloadSampleExercises: true,
+      info: {
+        created: '2018-10-12T02:30:10.370Z',
+        modified: '2018-10-12T02:31:14.562Z',
+        lastTrainedAt: null
       }
     }
   }
