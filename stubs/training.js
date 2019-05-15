18420d37f270365f69ad9379a533803ba1e1c218
x700700@gmail.com <booli@feature.fm>
2019-05-15 12:14:10 +0300

    stubs

---
+++ stubs/training.js
@@ -0,0 +1,87 @@
+
+const trainingStub = {
+  "id": "5bc2eaf4d71c15c14d0ec30a",
+  "name": "xdghdg",
+  "created": "2018-10-14T07:06:28.172Z",
+  "modified": "2018-10-14T07:06:50.666Z",
+  "popularity": 4,
+  "difficulty": 2,
+  "exercisesCount": 5,
+  "info": {
+    "userId": "5bc006e5457bb9bf8d7fe0a6",
+    "archived": false,
+    "lastTrainedAt": null
+  },
+  "exercises": {
+    "5bc2eb04d71c15c14d0ec30b": {
+      "id": "5bc2eb04d71c15c14d0ec30b",
+      "trainingId": "5bc2eaf4d71c15c14d0ec30a",
+      "question": "",
+      "answer": "",
+      "played": 0,
+      "wrongs": 0,
+      "shouldReloadSampleExercises": true,
+      "info": {
+        "created": "2018-10-14T07:06:44.689Z",
+        "modified": "2018-10-14T07:06:50.666Z",
+        "lastTrainedAt": null
+      }
+    },
+    "5bc2eb04d71c15c14d0ec30d": {
+      "id": "5bc2eb04d71c15c14d0ec30d",
+      "trainingId": "5bc2eaf4d71c15c14d0ec30a",
+      "question": "",
+      "answer": "",
+      "played": 0,
+      "wrongs": 0,
+      "shouldReloadSampleExercises": true,
+      "info": {
+        "created": "2018-10-14T07:06:44.830Z",
+        "modified": "2018-10-14T07:06:50.666Z",
+        "lastTrainedAt": null
+      }
+    },
+    "5bc2eb04d71c15c14d0ec30f": {
+      "id": "5bc2eb04d71c15c14d0ec30f",
+      "trainingId": "5bc2eaf4d71c15c14d0ec30a",
+      "question": "hg",
+      "answer": "",
+      "played": 0,
+      "wrongs": 0,
+      "shouldReloadSampleExercises": true,
+      "info": {
+        "created": "2018-10-14T07:06:44.965Z",
+        "modified": "2018-10-14T07:06:50.666Z",
+        "lastTrainedAt": null
+      }
+    },
+    "5bc2eb05d71c15c14d0ec311": {
+      "id": "5bc2eb05d71c15c14d0ec311",
+      "trainingId": "5bc2eaf4d71c15c14d0ec30a",
+      "question": "ghg",
+      "answer": "jkjk",
+      "played": 0,
+      "wrongs": 0,
+      "shouldReloadSampleExercises": true,
+      "info": {
+        "created": "2018-10-14T07:06:45.111Z",
+        "modified": "2018-10-14T07:06:50.666Z",
+        "lastTrainedAt": null
+      }
+    },
+    "5bc2eb05d71c15c14d0ec313": {
+      "id": "5bc2eb05d71c15c14d0ec313",
+      "trainingId": "5bc2eaf4d71c15c14d0ec30a",
+      "question": "gfhg",
+      "answer": "ghgh",
+      "played": 0,
+      "wrongs": 0,
+      "shouldReloadSampleExercises": true,
+      "info": {
+        "created": "2018-10-14T07:06:45.253Z",
+        "modified": "2018-10-14T07:06:50.666Z",
+        "lastTrainedAt": null
+      }
+    }
+  }
+};
